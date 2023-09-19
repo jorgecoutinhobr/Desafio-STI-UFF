08bfd7e6da15527e6b7f1bb63c5c71a01d4eb32b
Jorge Coutinhno <jorgecsn@id.uff.br>
2023-09-19 18:55:24 -0300

    Retirei variável desnecessária

--- main.rb
+++ main.rb
@@ -2,4 +2,4 @@ require './inicializar'
 
 dados_aluno = Inicializar.dados_aluno
 aluno = Inicializar.aluno(dados_aluno)
-aluno_novo_email = Inicializar.novo_email(aluno)
+Inicializar.novo_email(aluno)
